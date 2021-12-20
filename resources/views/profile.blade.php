83a69995069ee533d5f1d7eb2f791d1ee8119893
Crystallizee <benidictus.tf@yahoo.com>
2021-12-20 15:43:05 +0700

    ++

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -22,7 +22,7 @@
         <li><a href="#about-me">About Me</a></li>
         <li><a href="#certificate">Certificate</a></li>
         <li><a href="#portofolio">My Portfolio</a></li>
-        <li><a href="#contact-us">Contact Me</a></li>
+        <li><a href="#contact-me">Contact Me</a></li>
     </ul>
     <!-- End Header -->
     {{-- content --}}
@@ -30,7 +30,7 @@
         {{-- about me --}}
         <div id="about-me">
             <div class="section-introduction">
-                <img src="assets\img\111.png" alt="beni" class="img-icon">
+                <img src="assets\img\beni.png" alt="beni" class="img-icon">
                 <p style="font-size: 20pt">Benidictus Tri Wibowo</p>
                 <p class="content-perkenalan">Beni adalah seorang web developer yang berfokuskan di front end developer dan membuat website yang modern dan responsive.</p>
                 
@@ -88,12 +88,38 @@
             {{-- portfolio --}}
             <div id="portofolio">
                 <p style="font-size: 20pt">My Portfolio</p>
-
-                <iframe src="https://websiteutsweb.000webhostapp.com/index.php" frameborder="0" class="mywebsite"></iframe>
-                <iframe src="http://uaspawatma.xyz" frameborder="0" class="mywebsite"></iframe>
-                
+                <a href="https://websiteutsweb.000webhostapp.com/index.php">MY WEBSITE</a><br>
+                <iframe src="https://websiteutsweb.000webhostapp.com/index.php" frameborder="0" class="mywebsite"></iframe><br>
+                <a href="http://uaspawatma.xyz">MY WEBSITE</a><br>
+                <iframe src="http://uaspawatma.xyz" frameborder="0" class="mywebsite">my website</iframe>
             </div>
             {{-- End of portfolio --}}
+            {{-- Contact Me --}}
+            <div id="contact-me">
+                <p style="font-size: 20pt">CONTACT ME</p>
+                <div class="row">
+                    <div class="col">
+                        <h3>Email</h3>
+                        <a href="mailto:someone@example.com" class="contact-icon-bundler" style="text-decoration: none">
+                            <img src="assets/img/mail.png" alt="mail.png" class="contact-icon" width="100px">
+                            <h2 class="contact-desktop">benidictustriwibowo@gmail.com</h2>
+                        </a>
+                        <a href="tel:+62 851-5522-1292" class="contact-icon-bundler">
+                            <img src="assets/img/call.png" alt="phone.png" class="contact-icon" width="100px" style="text-decoration: none">
+                            <h2 class="contact-desktop">+62 851-5522-1292</h2>
+                        </a>
+                    </div>
+                    <div class="col">
+                        <h3>Temukan saya di </h3>
+                        <a href="https://www.instagram.com/benidictus_/" style="text-decoration: none"><ul><img src="assets/img/instagram.png" alt="IG" class="img-contact"> Instagram</ul></a>
+                        <a href="https://www.facebook.com/slazr.blood" style="text-decoration: none"><ul><img src="assets/img/fb.png" alt="FB" class="img-contact"> Facebook</ul></a>
+                        <a href="https://github.com/Crystallizee" style="text-decoration: none"><ul><img src="assets/img/github.png" alt="GH" class="img-contact"> Github</ul></a>
+                        <a href="https://www.linkedin.com/in/benidictus-tri-wibowo-23712b220/" style="text-decoration: none"><ul><img src="assets/img/linkedin.png" alt="Linked in" class="img-contact"> Linked In</ul></a>
+
+                    </div>
+                </div>
+            </div>
+            {{-- End ofContact Me --}}
         </div>
         {{-- End about me --}}
     </div>
